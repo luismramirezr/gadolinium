61f93adc40fcb982f41c8bdb6acb09734904a7e2
Luis Ramirez <luis@codinglabs.dev>
2020-11-29 12:18:09 -0300

    feat/(web-client): add api basic workflow

---
+++ services/web-client/utils/localStorage.ts
@@ -0,0 +1,40 @@
+import { LOCAL_STORAGE_PREFIX } from 'config/constants';
+
+export const getLocalStorageKey = (key: string) =>
+  `${LOCAL_STORAGE_PREFIX}/${key}`;
+
+export const getLocalStorageItem = (rawKey: string, parse = true) => {
+  const key = getLocalStorageKey(rawKey);
+  try {
+    const data = localStorage.getItem(key);
+    if (data && parse) return JSON.parse(data);
+    return data;
+  } catch (e) {
+    localStorage.removeItem(key);
+    return null;
+  }
+};
+
+export const saveLocalStorageItem = (
+  rawKey: string,
+  data: any,
+  parse = true
+) => {
+  try {
+    const key = getLocalStorageKey(rawKey);
+    localStorage.setItem(key, parse ? JSON.stringify(data) : data);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const removeLocalStorageItem = (rawKey: string) => {
+  const key = getLocalStorageKey(rawKey);
+  localStorage.removeItem(key);
+};
+
+export const isLocaltStorageItemSet = (rawKey: string) => {
+  const key = getLocalStorageKey(rawKey);
+  const data = localStorage.getItem(key);
+  return !!data && !!data.length;
+};
