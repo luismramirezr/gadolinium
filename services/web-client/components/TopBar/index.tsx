171f5e6449f039c09efd701ff71b4c23dec97d5a
Luis Ramirez <luis@codinglabs.dev>
2020-11-29 13:21:03 -0300

    feat/(web-client): add TopBar LogIn component

--- services/web-client/components/TopBar/index.tsx
+++ services/web-client/components/TopBar/index.tsx
@@ -4,13 +4,17 @@ import UserAvatar from 'components/UserAvatar';
 import Menu from 'components/Menu';
 import CartMenu from 'components/CartMenu';
 
+import { useRootState } from 'store/util/useRootState';
+import { isAuth } from 'store/Authentication/selectors';
+import TopBarLogIn from './components/TopBarLogIn';
+
 const TopBar: React.FC = () => {
+  const state = useRootState();
+  const isAuthenticated = isAuth(state);
   return (
     <Box py={2}>
       <Grid container justify="space-between" alignItems="center">
-        <Grid item>
-          <UserAvatar />
-        </Grid>
+        <Grid item>{isAuthenticated ? <UserAvatar /> : <TopBarLogIn />}</Grid>
         <Grid item>
           <Grid item container>
             <Grid item>
--- services/web-client/components/UserAvatar/index.tsx
+++ services/web-client/components/UserAvatar/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Avatar } from '@material-ui/core';
 
+import { useRootState } from 'store/util/useRootState';
+
 import useStyles from './styles';
+import { getCustomer } from 'store/Authentication/selectors';
 
 const UserAvatar = () => {
+  const state = useRootState();
+  const customer = getCustomer(state);
+
+  if (!customer) return null;
+
+  const { avatar, name, surname } = customer;
+
+  const initials = `${name[0]}${surname[0]}`;
+
   const classes = useStyles();
-  return <Avatar className={classes.avatar}>LR</Avatar>;
+  return (
+    <Avatar className={classes.avatar} src={avatar}>
+      {initials}
+    </Avatar>
+  );
 };
 
 export default UserAvatar;
